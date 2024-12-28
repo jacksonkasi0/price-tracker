1f162461d16670c3750f58d3d8933273b2b133aa
jacksonkasi0 <jacksonkasi@outlook.com>
2024-12-28 21:16:18 +0530

    Remove obsolete API routes and implement new webhook for price updates

--- src/app/api/webhook/update-prices/route.ts
+++ src/app/api/webhook/update-prices/route.ts
@@ -8,7 +8,6 @@ import { db } from "@/db";
 import { productsTable, priceHistoryTable } from "@/db/schema";
 
 // ** import utils
-import { fetchPrice } from "@/utils/scraper";
 import { sendSms } from "@/utils/alerts/send-sms";
 import { sendEmail } from "@/utils/alerts/send-mail";
 
@@ -17,7 +16,15 @@ export const runtime = 'edge';
 // https://price-tracker-1sh.pages.dev/api/update-prices
 export const POST = async (request: Request) => {
 
-  console.log("Request received", request.body);
+   // Parse the request body
+   const body = await request.json(); // Reads and parses the JSON body
+  
+   console.log("Request received:", body); 
+  //  @ts-ignore
+   console.log("Request Length :", body?.length!); 
+  //  Request received: { snapshot_id: 's_m5748p0y2mvjwp6wi2', status: 'ready' }
+
+  // curl -H "Authorization: Bearer 08b6677783e615a48d917be748feb23faf4237047edcddc2815587b81ac7398e" "https://api.brightdata.com/datasets/v3/snapshot/s_m5748p0y2mvjwp6wi2?format=json"
 
   // Initialize Cloudflare KV binding
   const kv = getRequestContext().env.PRICE_TRACKER;
@@ -26,7 +33,7 @@ export const POST = async (request: Request) => {
 
   await Promise.all(
     allProducts.map(async (product) => {
-      const priceData = await fetchPrice(product.url); // TODO: Implement scraper
+      const priceData = await downloadProductData(product.url); // TODO
       const current_price = priceData.price;
       const productData = priceData.product_data;
 
@@ -40,6 +47,7 @@ export const POST = async (request: Request) => {
       await db.insert(priceHistoryTable).values({
         product_id: product.id,
         price: current_price,
+        snapshot_id: product.last_snapshot_id,
       });
 
       // Get existing product data from KV (optional)
